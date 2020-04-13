b058759c80d2bd7cb9f8867ba62b6821a64cbbd2
jaswanthrk <jaswanthayrus@gmail.com>
2020-04-14 01:34:36 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// var button = document.getElementById("enter");
-// var input  = document.getElementById("userinput");
-// var ul     = document.querySelector("ul");
-// var listItems = document.getElementsByClassName('list-item')
 var chngbutton = document.getElementById("colorbtn")
 var navbar = document.getElementById("main-navigation")
 var pagebg = document.getElementById('page-bg')
@@ -10,16 +6,11 @@ var drpdwn = document.getElementById("dropdownMenuLink")
 var drpdwn2 = document.getElementsByClassName("navbar-nav")[0].getElementsByClassName("dropdown-menu")[0]
 var drdnlnks= drpdwn2.children
 var jumbo  = document.getElementById("jumbo-part")
+var content= document.getElementsByClassName("all-container")[0]
 
 function changeTheColor() {
 	if (i % 2 === 1) {
 		console.log('hellooo')
-		// document.body.style.background = 'rgb(75,17,101)';
-		// document.body.style.background = 'radial-gradient(circle, rgba(75,17,101,1) 18%, rgba(53,56,64,1) 100%)'
-		// document.body.style.background = 'rgb(0,129,147)';
-		// document.body.style.background = 'radial-gradient(circle, rgba(0,129,147,1) 0%, rgba(0,0,0,1) 100%)'
-		// document.body.style.background = 'url(images/img1.jpg) no-repeat center center fixed';
-		// zone.style.background   = "#b41819";
 		zone.style.background   = "#000000";
 
 		pagebg.style.background = '#262626';
@@ -35,43 +26,22 @@ function changeTheColor() {
 		drpdwn2.classList.remove("drp-light");
 		drpdwn2.classList.add("drp-dark");
 
+		if(jumbo != null){
 		jumbo.classList.remove("light-text")
 		jumbo.classList.add("dark-text")
+		}
+
+		content.classList.remove("light-text")
+		content.classList.add("dark-text")
 
 		for (var j = drdnlnks.length - 1; j >= 0; j--) {
 		 	drdnlnks[j].classList.remove("light-mode-links")
 		 	drdnlnks[j].classList.add("dark-mode-links")
 		}		
-
-		// document.body.style.background = 'rgb(187,252,255)';
-		// document.body.style.background = 'radial-gradient(circle, rgba(187,252,255,1) 0%, rgba(27,255,94,1) 34%, rgba(219,193,2,1) 57%, rgba(254,107,107,1) 90%) no-repeat center center fixed';
 	}
 	else { // LIGHT MODE
-		// document.body.style.background = 'rgb(255,255,228)';
-		// document.body.style.background = 'radial-gradient(circle, rgba(203,239,255,1) 8%, rgba(255,182,182,1) 100%)';
-		// document.body.style.background = 'rgb(255,193,193)';
-		// document.body.style.background = 'radial-gradient(circle, rgba(255,193,193,1) 24%, rgba(254,254,254,1) 91%, rgba(255,178,178,1) 100%)';
-		// document.body.style.background = 'rgb(255,139,139)';
-		// document.body.style.background = 'radial-gradient(circle, rgba(255,139,139,1) 0%, rgba(236,206,100,1) 24%, rgba(255,230,54,1) 43%, rgba(106,255,154,1) 100%, rgba(255,255,255,1) 100%)';
-
-		// document.body.style.background = 'rgb(255,240,187)';
-		// document.body.style.background = 'radial-gradient(circle, rgba(255,240,187,1) 24%, rgba(255,243,162,1) 43%, rgba(255,255,255,1) 100%)';
-		// document.body.style.background = '#ffffff';
-		// document.body.style.background = 'img/img1.jpg'
-		// document.body.style.background = 'radial-gradient(circle, rgba(255,197,197,1) 0%, rgba(255,255,255,1) 100%)';
-		// pagebg.style.background = 'url(images/img4.jpg) no-repeat center center fixed';
-		// zone.style.background   = "#ed6767";
-		// zone.style.background   = "#7d8993";
-		// zone.style.background   = "#d1a561";
-		// zone.style.background   = "#9d766c";
-		// zone.style.background   = "#d1a561";
-		// zone.style.background   = "#d1a561";
-		// zone.style.background   = "#d1a561";
-		// zone.style.background   = "#E5998E";
 		zone.style.background   = "#ffffff";
 		pagebg.style.background = '#EEF2F4';
-
-		// drpdwn.style.backgroundColor = "#ffffff";
 
 		chngbutton.classList.remove("btn-outline-light");
 		chngbutton.classList.add("btn-outline-dark");
@@ -82,16 +52,18 @@ function changeTheColor() {
 		drpdwn2.classList.add("drp-light");
 		drpdwn2.classList.remove("drp-dark");
 
+		if(jumbo != null){
 		jumbo.classList.remove("dark-text")
 		jumbo.classList.add("light-text")
+		}
+
+		content.classList.remove("dark-text")
+		content.classList.add("light-text")
 
 		for (var j = drdnlnks.length - 1; j >= 0; j--) {
 		 	drdnlnks[j].classList.remove("dark-mode-links")
 		 	drdnlnks[j].classList.add("light-mode-links")
 		}	
-
-		// document.body.style.background = 'rgb(238,174,202)';
-  		// document.body.style.background = 'linear-gradient(90deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 82%) no-repeat center center fixed';
 	}
 	i = i + 1; 
 	console.log(i)
